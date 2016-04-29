f9a3342cb574f1e3aff75df98882fddb0cdfa6cb
Agnieszka <agagancarczyk@gmail.com>
2016-04-29 23:55:04 +0100

    added starter content to freinds and reports views

--- app/friends/controllers/friends-controller.js
+++ app/friends/controllers/friends-controller.js
@@ -12,8 +12,21 @@
     .module('friends')
     .controller('FriendsCtrl', FriendsCtrl);
 
-  function FriendsCtrl() {
+  function FriendsCtrl(Users, $cookies) {
     var vm = this;
     vm.ctrlName = 'FriendsCtrl';
+    vm.userProfile = '';
+
+    /*
+     * Get User's Profile
+     */
+    Users.getUserProfile().success(function (data) {
+      vm.userProfile = data;
+      vm.location = vm.userProfile.location;
+      vm.bDate = new Date(vm.userProfile.birthday);
+      vm.bMnth = ("0" + (vm.bDate.getMonth()+1)).slice(-2),
+      vm.bDay  = ("0" + vm.bDate.getDate()).slice(-2);
+      vm.fullBDate = vm.bDay + '-' + vm.bMnth + '-' + vm.bDate.getFullYear();
+    });
   }
 }());
--- app/reports/controllers/reports-controller.js
+++ app/reports/controllers/reports-controller.js
@@ -12,8 +12,21 @@
     .module('reports')
     .controller('ReportsCtrl', ReportsCtrl);
 
-  function ReportsCtrl() {
+  function ReportsCtrl(Users, $cookies) {
     var vm = this;
     vm.ctrlName = 'ReportsCtrl';
+    vm.userProfile = '';
+
+    /*
+     * Get User's Profile
+     */
+    Users.getUserProfile().success(function (data) {
+      vm.userProfile = data;
+      vm.location = vm.userProfile.location;
+      vm.bDate = new Date(vm.userProfile.birthday);
+      vm.bMnth = ("0" + (vm.bDate.getMonth()+1)).slice(-2),
+      vm.bDay  = ("0" + vm.bDate.getDate()).slice(-2);
+      vm.fullBDate = vm.bDay + '-' + vm.bMnth + '-' + vm.bDate.getFullYear();
+    });
   }
 }());
