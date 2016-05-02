5ddfaee83a3b190170f64b1f9d8b4d78f89e1244
Agnieszka <agagancarczyk@gmail.com>
2016-05-02 15:38:19 +0100

    added bar charts to reports view

--- app/reports/controllers/reports-controller.js
+++ app/reports/controllers/reports-controller.js
@@ -20,7 +20,12 @@
     vm.walkingActivities = [];
     vm.runningActivities = [];
     vm.cyclingActivities = [];
-
+    vm.chartDatesRunning = [];
+    vm.chartDistanceRunning = [];
+    vm.chartDatesWalking = [];
+    vm.chartDistanceWalking = [];
+    vm.chartDatesCycling = [];
+    vm.chartDistanceCycling = [];
     /*
      * Get User's Profile
      */
@@ -35,37 +40,73 @@
       vm.activities = data.items;
     });
 
-      vm.running = function () {
-        vm.current = 'Running';
-        vm.runningActivities = [];
-        for (var i = 0; i < vm.activities.length; i++){
-          if (vm.activities[i].type === 'Running'){
-            vm.runningActivities.push(vm.activities[i]);
-         }
-       }
-       console.log(vm.runningActivities);
+    vm.running = function () {
+      vm.current = 'Running';
+      vm.runningActivities = [];
+      vm.chartDatesRunning = [];
+      vm.chartDistanceRunning = [];
+
+      for (var i = 0; i < vm.activities.length; i++){
+        if (vm.activities[i].type === 'Running'){
+          vm.runningActivities.push(vm.activities[i]);
+        }
       };
 
-      vm.walking = function () {
-        vm.current = 'Walking';
-        vm.walkingActivities = [];
-        for (var i = 0; i < vm.activities.length; i++){
-          if (vm.activities[i].type === 'Walking'){
-            vm.walkingActivities.push(vm.activities[i]);
-         }
-       }
-       console.log(vm.walkingActivities);
+      for (var j = 0; j < vm.runningActivities.length; j++){
+        vm.chartDatesRunning.push(vm.runningActivities[j].start_time.slice(4,16));
+        vm.chartDistanceRunning.push((vm.runningActivities[j].total_distance.toFixed())/1000);
       };
 
-      vm.cycling = function () {
-        vm.current = 'Cycling';
-        vm.cyclingActivities = [];
-        for (var i = 0; i < vm.activities.length; i++){
-          if (vm.activities[i].type === 'Cycling'){
-            vm.cyclingActivities.push(vm.activities[i]);
-         }
-       }
-       console.log(vm.cyclingActivities);
+      $rootScope.labels = vm.chartDatesRunning;
+      $rootScope.data = [
+        vm.chartDistanceRunning
+      ];
+    };
+
+    vm.walking = function () {
+      vm.current = 'Walking';
+      vm.walkingActivities = [];
+      vm.chartDatesWalking = [];
+      vm.chartDistanceWalking = [];
+
+      for (var i = 0; i < vm.activities.length; i++){
+        if (vm.activities[i].type === 'Walking'){
+          vm.walkingActivities.push(vm.activities[i]);
+        }
       };
+
+      for (var j = 0; j < vm.walkingActivities.length; j++){
+        vm.chartDatesWalking.push(vm.walkingActivities[j].start_time.slice(4,16));
+        vm.chartDistanceWalking.push((vm.walkingActivities[j].total_distance.toFixed())/1000);
+      };
+
+      $rootScope.labels = vm.chartDatesWalking;
+      $rootScope.data = [
+        vm.chartDistanceWalking
+      ];
+    };
+
+    vm.cycling = function () {
+      vm.current = 'Cycling';
+      vm.cyclingActivities = [];
+      vm.chartDatesCycling = [];
+      vm.chartDistanceCycling = [];
+
+      for (var i = 0; i < vm.activities.length; i++){
+        if (vm.activities[i].type === 'Cycling'){
+          vm.cyclingActivities.push(vm.activities[i]);
+        }
+      };
+
+      for (var j = 0; j < vm.cyclingActivities.length; j++){
+        vm.chartDatesCycling.push(vm.cyclingActivities[j].start_time.slice(4,16));
+        vm.chartDistanceCycling.push((vm.cyclingActivities[j].total_distance.toFixed())/1000);
+      };
+
+      $rootScope.labels = vm.chartDatesCycling;
+      $rootScope.data = [
+        vm.chartDistanceCycling
+      ];
+    };
   }
 }());
